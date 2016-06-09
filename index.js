437671472b893372e852fc739f9e6ba55fc78d27
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-06-09 19:55:46 -0400

    First html home page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var server = express();
 
 
 server.get('/', function(request , response){
-  response.send('Hello, I am finally here!');
+  response.sendFile('public/html/index.html', {root: __dirname});
 });
 
 server.listen(8080);
